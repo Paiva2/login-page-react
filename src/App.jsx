f3a8eaf8543d3e33d789a2923e872d3c4c4d72e6
Paiva <joaovitor.paiva145@hotmail.com>
2023-03-11 01:33:01 -0300

    new component: routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,7 @@
-import Login from "./components/Login";
-import Register from "./components/Register";
-import { Route, Routes, BrowserRouter, Navigate } from "react-router-dom";
-import Home from "./components/Home";
-import { useSelector } from "react-redux";
-import NotFound from "./components/NotFound";
-import ForgotPassword from "./components/ForgotPassword";
-import Modal from "react-modal/lib/components/Modal";
+import MainRoutes from "./routes/MainRoutes";
 
 function App() {
-  const authorized = useSelector((state) => state.userData.authorized);
-
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Navigate to="/login" />} />
-        <Route path="/login" element={!authorized ? <Login /> : <Home />} />
-        <Route path="/register" element={<Register />} />
-        <Route
-          path="/home"
-          element={authorized ? <Home /> : <Navigate to="/login" />}
-        />
-        <Route path="/forgot-password" element={<ForgotPassword />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
-  );
+  return <MainRoutes />;
 }
 
 export default App;
-Modal.setAppElement("#root");
---
+++ src/routes/MainRoutes.jsx
@@ -0,0 +1,29 @@
+import { Route, Routes, BrowserRouter, Navigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import Login from "../components/Login";
+import Register from "../components/Register";
+import Home from "../components/Home";
+import NotFound from "../components/NotFound";
+import ForgotPassword from "../components/ForgotPassword";
+
+function MainRoutes() {
+  const authorized = useSelector((state) => state.userData.authorized);
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" />} />
+        <Route path="/login" element={!authorized ? <Login /> : <Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route
+          path="/home"
+          element={authorized ? <Home /> : <Navigate to="/login" />}
+        />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default MainRoutes;
