bc05876b7db83d3fff5d4ea10bc576d95f9ae45a
unknown <joaovitor.paiva145@hotmail.com>
2023-03-16 14:32:02 -0300

    styles: added mobile styles to home list

--- src/components/PlaceHolder.jsx
+++ src/components/PlaceHolder.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { BsChatText } from "react-icons/bs";
+import "../styles/PlaceHolder.css";
 
 const PlaceHolder = () => {
   return (
-    <div>
-      Post something! <BsChatText />
+    <div className="place-holder-text">
+      <h3>
+        Post something! <BsChatText />
+      </h3>
     </div>
   );
 };
